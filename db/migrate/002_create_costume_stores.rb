5cab2f99bfb11fc4c8c364bf03a204ea69f4730f
Garrett Thompson <gtcruz.gt@gmail.com>
2020-08-20 23:15:49 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,11 +1,14 @@
 # Create your costume_stores migration here
 class CreateCostumeStores < ActiveRecord::Migration
   def change
-    create_table :costumes do |t|
-      t.string :name
-      t.integer :price
-      t.integer :size
-      t.string :image
+    create_table :costumeStores do |t|
+      t.XXX :name
+      t.XXX :location
+      t.XXX :costumeInventory
+      t.XXX :numEmployees
+      t.XXX :inBusiness
+      t.XXX :open
+      t.XXX :closing
     end
   end
 end
