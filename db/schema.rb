c4bb62bd0665ad239398faeafa8337c339573d94
Garrett Thompson <gtcruz.gt@gmail.com>
2020-08-20 23:46:55 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -1,33 +1,15 @@
-ActiveRecord::Schema.define(version: 3) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
-  create_table "costume_stores", force: :cascade do |t|
-    t.string :name
-    t.string :location
-    t.integer :costumeInventory
-    t.integer :numEmployees
-    t.boolean :inBusiness
-    t.datetime :open
-    t.datetime :closing
-  end
-
-  create_table "costumes", force: :cascade do |t|
-    t.string :name
-    t.float :price
-    t.string :image
-    t.string :size
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "haunted_houses", force: :cascade do |t|
-    t.string :name
-    t.string :location
-    t.string :theme
-    t.float :price
-    t.boolean :familyFriendly
-    t.datetime :openDate
-    t.datetime :closingDate
-    t.text :description
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
