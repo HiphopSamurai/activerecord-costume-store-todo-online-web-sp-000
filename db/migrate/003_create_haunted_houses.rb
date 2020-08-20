1c55e335c5657b401eadcda7ab47d385e64ca6c0
Garrett Thompson <gtcruz.gt@gmail.com>
2020-08-20 23:13:34 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,4 +1,11 @@
 # Create your haunted_houses migration here
 class HauntedHouses < ActiveRecord::Migration
-
+  def change
+    create_table :costumes do |t|
+      t.string :name
+      t.integer :price
+      t.integer :size
+      t.string :image
+    end
+  end
 end
